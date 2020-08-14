18835b880ae09b8a61087c17cba559fcd7f5aeac
idrees04 <idreesar@yahoo.com>
2020-08-14 15:15:41 +0500

    UPDATE DATA IN DATABASE

--- EFCodeFirstApproach/Controllers/HomeController.cs
+++ EFCodeFirstApproach/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
 
 namespace EFCodeFirstApproach.Controllers
 {
@@ -37,7 +38,7 @@ namespace EFCodeFirstApproach.Controllers
                 {
                    // ViewBag.InsertMewssage = "<script>alert('Data Inserted !!')</script>";
                    // TempData["InsertMewssage"] = "<script>alert('Data Inserted !!')</script>";
-                    TempData["InsertMewssage"] = "Data Inserted !!";
+                    TempData["InsertMewssage"] = "Data Inserted ";
                     return RedirectToAction("autoGenratedView");
                     //ModelState.Clear();
                 }
@@ -46,8 +47,43 @@ namespace EFCodeFirstApproach.Controllers
                     ViewBag.InsertMewssage = "<script>alert('Data not Inserted !!')</script>";
                 }
             }
-            
             return View();
+        }
+        public ActionResult Edit(int id)
+        {
+            // if model of id is equal to database of student id then  FirstOrDefault() method will store row in (var row).
+            var row = db.students.Where(model => model.Id == id).FirstOrDefault();
+            return View(row);
+        }
+        [HttpPost]
+        public ActionResult Edit(Student s)
+        {
+            if (ModelState.IsValid == true)
+            {
+                // this method will run wihen we will click on button
+
+                db.Entry(s).State = EntityState.Modified;
+                int a = db.SaveChanges();
+                if (a > 0)
+                {
+                    //ViewBag.UpdateMessage = "<script>alert('Data Updated')</script>";
+                    // ModelState.Clear();
+                    TempData["UpdateMewssage"] = "Data Updated ";
+                    return RedirectToAction("autoGenratedView");
+                }
+                else
+                {
+                    ViewBag.UpdateMessage = "<script>alert('Data Not Updated')</script>";
+                }
+            }
+
+
+
+
+
+
+
+                return View();
         }
     }
 }
